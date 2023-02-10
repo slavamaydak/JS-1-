31abd09534ad395d3b07d57dff3d1f103449a268
slavamaydak <108940804+slavamaydak@users.noreply.github.com>
2023-02-10 17:09:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// alert("hello world"); - кнопка з "ок"
+
+//let number; // змінна
+// number = 123;
+
+// alert(number);
+
+// let a = 23;
+// let b = "hello world!";
+
+// alert(a);
+// alert(b);
+
+// let rezoult = 2 * 4;
+// alert(rezoult);
+
+// alert(2 * 4);
+
+// let a = "Helo";
+// let username = "Slava";
+
+// let r = a + " " + username;
+// alert(r);
+
+// prompt ("Enter your Name");
+
+let a = 3;
+let b = 5;
+alert(a + a + b + b);
+alert(a * b);
+
+let c = 5;
+alert(((c + 10) * 20 - 30) / 5);
